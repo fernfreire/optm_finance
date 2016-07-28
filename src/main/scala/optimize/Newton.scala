5b64260cdfe5dbc714949d7362555e63af528a80
Fernando Freire <fernfreire@gmail.com>
2016-07-27 21:23:31 -0300

    newton beta

--- src/main/scala/optimize/Newton.scala
+++ src/main/scala/optimize/Newton.scala
@@ -3,6 +3,14 @@ package optimize
 import breeze.linalg._
 import breeze.numerics._
 
-class Newton {
-  
+object Newton {
+  def findMin(gradF: DenseVector[Double] => DenseVector[Double], H: DenseVector[Double] => DenseMatrix[Double], initialX: DenseVector[Double], eps: Double): DenseVector[Double] = {
+    def loop(gradF: DenseVector[Double] => DenseVector[Double], H: DenseVector[Double] => DenseMatrix[Double], currentX: DenseVector[Double], eps: Double, d: DenseVector[Double]): DenseVector[Double] = {
+      if(norm(d) < eps)
+        currentX
+      else
+        loop(gradF, H, currentX + d, eps, -((inv(H(currentX))) * (gradF(currentX))))
+    }
+    loop(gradF, H, initialX, eps, -((inv(H(initialX))) * (gradF(initialX))))
+  }
 }
--- src/main/scala/optimize/RunOptimization.scala
+++ src/main/scala/optimize/RunOptimization.scala
@@ -5,18 +5,31 @@ import breeze.numerics._
 import java.io.File
 
 object RunOptimization extends App {
-  val paths = Map(1 -> "/Users/fernandofreiredemedeiros/MSc/MAC5796/terminal/optm_finance/src/main/scala/optimize/prices.csv",
-    2 -> "c:/Users/fernanda/desktop/fernando/MAC5796/project/optm_finance/src/main/scala/optimize/prices.csv")
-  val choose = 2
-  val path = paths(choose)
-  val file = new File(path)
-  val myEstimator = new Estimator(file, 1.0 / 252)
-  val uoODE = new OrnsteinUhlenbeckODE(myEstimator.theta, myEstimator.avg, myEstimator.vol, 12.15)
-  val rkSolver = new RungeKuttaSolver[OrnsteinUhlenbeckODE](uoODE, 20, DenseVector(1.0), 0e0, 1e0)
+  // val paths = Map(1 -> "/Users/fernandofreiredemedeiros/MSc/MAC5796/terminal/optm_finance/src/main/scala/optimize/prices.csv",
+  //   2 -> "c:/Users/fernanda/desktop/fernando/MAC5796/project/optm_finance/src/main/scala/optimize/prices.csv")
+  // val choose = 2
+  // val path = paths(choose)
+  // val file = new File(path)
+  // val myEstimator = new Estimator(file, 1.0 / 252)
+  // val uoODE = new OrnsteinUhlenbeckODE(myEstimator.theta, myEstimator.avg, myEstimator.vol, 12.15)
+  // val rkSolver = new RungeKuttaSolver[OrnsteinUhlenbeckODE](uoODE, 20, DenseVector(1.0), 0e0, 1e0)
   // val rkSolver = new RungeKuttaSolver[OrnsteinUhlenbeckODE](uoODE, 20, DenseVector(2.0, 3.0), 0e0, 1e0)
-  val solved = rkSolver.solve
-  solved.foreach(println(_))
+  // val solved = rkSolver.solve
+  // solved.foreach(println(_))
   // println(solved(0.0))
   // println(solved(0.5))
   // println(solved(1.0))
+  def grad(x: DenseVector[Double]): DenseVector[Double] = {
+    val x1 = x(0)
+    val x2 = x(1)
+    DenseVector(1 / (1 - x1 - x2) - (1 / x1), 1 / (1 - x1 - x2) - (1 / x2))
+  }
+
+  def thisH(x: DenseVector[Double]): DenseMatrix[Double] = {
+    val x1 = x(0)
+    val x2 = x(1)
+    (pow(1 / (1 - x1 - x2), 2)) :* DenseMatrix((1.0, 1.0), (1.0, 1.0)) + (pow(1 / (x1), 2)) :* DenseMatrix((1.0, 0.0), (1.0, 0.0))
+  }
+  val x = Newton.findMin(grad, thisH, DenseVector(0.2, 0.2), 1e-10)
+  println(x)
 }
