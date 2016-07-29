58cce068b9c8f2ed8101e0a51fde62609cfcef47
Fernando Medeiros <fernfreire@gmail.com>
2016-07-29 00:28:49 -0300

    modifying bvp

--- src/main/scala/optimize/BVP.scala
+++ src/main/scala/optimize/BVP.scala
@@ -8,8 +8,11 @@ import breeze.numerics._
 // Otherwise the system might have an infinity number of solutions
 class BVP[T <: ODE](rkSolver: RungeKuttaSolver[T]) extends Interpolation {
   def solveBVP(x: DenseVector[Double], y: DenseVector[Double], h: Int): Map[Double, DenseVector[Double]] = {
-    val (initialX, finalX) = (min(x), max(x))
-    val blah = x.map(t => 2 * t)
+    if(x.length != y.length) throw new Error("x and y should have the same size!")
+    val (lowerX, upperX) = (min(x), max(x))
+    val dim = x.length
+    val eyeMatrix = DenseMatrix.eye[Double](dim)
+    val mat = DenseMatrix.horzcat(x.toDenseMatrix.t, y.toDenseMatrix.t, eyeMatrix)
     Map(1.0->DenseVector(1.0))
   }
 }
